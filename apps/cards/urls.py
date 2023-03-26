d9b317eee7674e1a1ee20b8c77de1c8536dedf32
utuedey <utuedeyejosph@gmail.com>
2023-03-26 21:55:11 +0100

    App Features - Modify urls.py (Add card-create,card-update urls) - Modify views.py (Add CardCreatView, CardUpdateView) - Modify card.html (Add card-update link) - Modify base.html (Add navigation template link) - Add template files (card_form.html, navigation.html)

--- apps/cards/urls.py
+++ apps/cards/urls.py
@@ -3,12 +3,26 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
+    # Url route for the landing page
     path("", TemplateView.as_view(template_name="cards/index.html"),   # url for the landing_page
           name="home"
           ),
+    # Url route for the card list page
     path(
         "cards",
         views.CardListView.as_view(),
         name="card-list"
+    ),
+    # Url route for the card create page
+    path(
+        "new",
+        views.CardCreateView.as_view(),
+        name="card-create"
+    ),
+    # Url route for the card update page
+    path(
+        "edit/<int:pk>",
+        views.CardUpdateView.as_view(),
+        name="card-update"
     ),
 ]
--- apps/cards/views.py
+++ apps/cards/views.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+from django.urls import reverse_lazy
 from django.views.generic import (
-    ListView
+    ListView,
+    CreateView,
+    UpdateView,
 )
 from .models import Card
 
@@ -13,3 +16,24 @@ class CardListView(ListView):
     """
     model = Card
     queryset = Card.objects.all().order_by("box", "-date_created")
+
+class CardCreateView(CreateView):
+    """A class-based view that creates a new card
+    Attributes:
+    - field: The fields the form would show
+           - question field
+           - answer field
+           - box field
+    - success_url: The url to return request to when the form is sent.
+    """
+    model = Card
+    fields = ["question", "answer", "box"]
+    success_url = reverse_lazy("card-create")
+
+class CardUpdateView(CardCreateView, UpdateView):
+    """A class-based view that inherits from CardCreateView and
+    UpdateView that update an exisitng card.
+    Attribute:
+    -success_url: The url to return request to when the form is sent
+    """
+    success_url = reverse_lazy("card-list")
